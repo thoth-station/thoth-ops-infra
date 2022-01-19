7a131b218b3f337692e46ca988e10df7464ca9f1
Christoph Görn <goern@redhat.com>
2022-01-19 08:33:04 +0100

    add a mass-approve script
    Signed-off-by: Christoph Görn <goern@redhat.com>

---
+++ gh_mass_approve.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+pr_to_be_approved=$(
+        gh api graphql --paginate -f query='
+{
+  search(query: "org:thoth-station in:title Enable TLS", type: ISSUE, first: 100) {
+    nodes {
+      ... on PullRequest {
+        number
+        repository {
+          name
+        }
+      }
+    }
+  }
+}
+' --jq .data.search.nodes
+)
+
+for k in $(jq '. | keys | .[]' <<<${pr_to_be_approved}); do
+        pr=$(jq -r ".[$k]" <<<${pr_to_be_approved})
+        pr_num=$(jq -r .number <<<${pr})
+        repo_name=$(jq -r .repository.name <<<${pr})
+        gh pr comment "https://github.com/thoth-station/$repo_name/pull/$pr_num" --body "/approve"
+done
+
+#end.
